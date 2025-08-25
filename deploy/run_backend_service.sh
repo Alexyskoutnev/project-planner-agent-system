e8e8f32b8a8e66e32daf683aa94eea90bbede824
Alexy Skoutnev <alexyskoutnev@gmail.com>
2025-08-25 16:17:26 -0500

    update

--- deploy/run_backend_service.sh
+++ deploy/run_backend_service.sh
@@ -15,7 +15,7 @@ After=network.target
 [Service]
 User=ubuntu
 WorkingDirectory=$APP_DIR
-ExecStart=$APP_DIR/venv/bin/uvicorn -w 4 -k uvicorn.workers.UvicornWorker api.main:app --bind 0.0.0.0:8000
+ExecStart=$APP_DIR/venv/bin/uvicorn api.main:app --bind 0.0.0.0:8000
 Restart=always
 
 [Install]
